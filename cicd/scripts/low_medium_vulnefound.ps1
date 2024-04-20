121e528c7975874188d116f92f357218f6aaefa7
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-04-20 13:03:55 -0300

    feature: introducing warn for low and medium vulnerabilities

---
+++ cicd/scripts/low_medium_vulnefound.ps1
@@ -0,0 +1,14 @@
+param (
+  [string]$lowLevel,
+  [string]$mediumLevel
+)
+
+Write-Warning "Low or Medium Vulnerabilities found, please be careful."
+Write-Output "`n"
+if ($lowLevel -eq "true") {
+  Write-Warning "[WARN]: LOW Severity Vulnerabilities Found."
+}
+if ($mediumLevel -eq "true") {
+  Write-Warning "[WARN]: MEDIUM Severity Vulnerabilities Found."
+}
+Write-Output "Check and update your packages to fix security. If you think that this is a mistake, contact the team at 'gustavo@gmail.com'.`nYou can discover more about OWASP Dependency Check in the official documentation at https://jeremylong.github.io/DependencyCheck/index.html"
