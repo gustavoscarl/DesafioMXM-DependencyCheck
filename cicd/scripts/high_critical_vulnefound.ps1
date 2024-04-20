2df3c45693397e01fbe8ae9147629b29a5e40a03
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-04-20 13:26:11 -0300

    fix: throwing back to write-output as write-error stop the code prematurely

--- cicd/scripts/high_critical_vulnefound.ps1
+++ cicd/scripts/high_critical_vulnefound.ps1
@@ -6,10 +6,10 @@ param (
 Write-Output "Vulnerabilities found, failing the build."
 Write-Output "`n"
 if ($highLevel -eq "true") {
-  Write-Error "HIGH Severity Vulnerabilities Found."
+  Write-Output "[FAIL]: HIGH Severity Vulnerabilities Found."
 }
 if ($criticalLevel -eq "true") {
-  Write-Error "CRITICAL Severity Vulnerabilities Found."
+  Write-Output "[FAIL]: CRITICAL Severity Vulnerabilities Found."
 }
 Write-Output "Check and update your packages to fix security. If you think that this is a mistake, contact the team at 'gustavo@gmail.com'.`nYou can discover more about OWASP Dependency Check in the official documentation at https://jeremylong.github.io/DependencyCheck/index.html"
 exit 1
