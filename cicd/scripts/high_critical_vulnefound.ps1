e389ad3e30d2c09966f6345bc52d0a2fd2ce5e86
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-04-20 13:30:53 -0300

    adding skip line to error

--- cicd/scripts/high_critical_vulnefound.ps1
+++ cicd/scripts/high_critical_vulnefound.ps1
@@ -11,5 +11,5 @@ if ($highLevel -eq "true") {
 if ($criticalLevel -eq "true") {
   Write-Output "[FAIL]: CRITICAL Severity Vulnerabilities Found."
 }
-Write-Output "Check and update your packages to fix security. If you think that this is a mistake, contact the team at 'gustavo@gmail.com'.`nYou can discover more about OWASP Dependency Check in the official documentation at https://jeremylong.github.io/DependencyCheck/index.html"
+Write-Output "`nCheck and update your packages to fix security. If you think that this is a mistake, contact the team at 'gustavo@gmail.com'.`nYou can discover more about OWASP Dependency Check in the official documentation at https://jeremylong.github.io/DependencyCheck/index.html"
 exit 1
