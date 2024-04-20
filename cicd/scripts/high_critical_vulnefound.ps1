853ce3d6c167f9f1397d84c3b40284b190e84447
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-04-19 23:06:46 -0300

    fix syntax in high_critical_vulne.ps1

--- cicd/scripts/high_critical_vulnefound.ps1
+++ cicd/scripts/high_critical_vulnefound.ps1
@@ -1,10 +1,15 @@
-          Write-Output "Vulnerabilities found, failing the build."
-          Write-Output "`n"
-          if ("${{ steps.check_vulnerabilities.outputs.highLevel }}" -eq "true"){
-            Write-Output "HIGH Severity Vulnerabilities Found."
-          }
-          if ("${{ steps.check_vulnerabilities.outputs.criticalLevel }}" -eq "true"){
-            Write-Output "CRITICAL Severity Vulnerabilities Found."
-          }
-          Write-Output "Check and update your packages to fix security."
-          exit 1
+param (
+  [string]$highLevel,
+  [string]$criticalLevel
+)
+
+Write-Output "Vulnerabilities found, failing the build."
+Write-Output "`n"
+if ($highLevel -eq "true") {
+  Write-Output "HIGH Severity Vulnerabilities Found."
+}
+if ($criticalLevel -eq "true") {
+  Write-Output "CRITICAL Severity Vulnerabilities Found."
+}
+Write-Output "Check and update your packages to fix security."
+exit 1
