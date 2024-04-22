7163f3b9f81f5eaa8b6043a110c38fa165a50553
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-04-22 13:09:10 -0300

    updated low_medium warn message

--- cicd/scripts/low_medium_vulnefound.ps1
+++ cicd/scripts/low_medium_vulnefound.ps1
@@ -11,4 +11,4 @@ if ($lowLevel -eq "true") {
 if ($mediumLevel -eq "true") {
   Write-Warning "MEDIUM Severity Vulnerabilities Found."
 }
-Write-Output "Check and update your packages to fix security. If you think that this is a mistake, contact the team at 'gustavo@gmail.com'.`nYou can discover more about OWASP Dependency Check in the official documentation at https://jeremylong.github.io/DependencyCheck/index.html"
+Write-Output "Check and update your packages to fix security. If you think that this is a mistake, contact the team at 'gustavo@gmail.com'.`nTo add vulnerabilities to suppression list (ignored list) you can set on ./cicd/dependency-check-config/suppression-backend.xml file. More info on FAQs in this repository README and in the official documentation.`nYou can discover more about OWASP Dependency Check in the official documentation at https://jeremylong.github.io/DependencyCheck/index.html"
