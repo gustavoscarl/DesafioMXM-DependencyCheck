00490880d393ba65d3d98764f155759e3954b372
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-04-22 13:07:39 -0300

    updated error message

--- cicd/scripts/high_critical_vulnefound.ps1
+++ cicd/scripts/high_critical_vulnefound.ps1
@@ -6,10 +6,10 @@ param (
 Write-Output "Vulnerabilities found, failing the build."
 Write-Output "`n"
 if ($highLevel -eq "true") {
-  Write-Output "[FAIL]: HIGH Severity Vulnerabilities Found."
+  Write-Output "[FAIL] HIGH Severity Vulnerabilities Found."
 }
 if ($criticalLevel -eq "true") {
-  Write-Output "[FAIL]: CRITICAL Severity Vulnerabilities Found."
+  Write-Output "[FAIL] CRITICAL Severity Vulnerabilities Found."
 }
-Write-Output "`nCheck and update your packages to fix security. If you think that this is a mistake, contact the team at 'gustavo@gmail.com'.`nYou can discover more about OWASP Dependency Check in the official documentation at https://jeremylong.github.io/DependencyCheck/index.html"
+Write-Output "`nCheck and update your packages to fix security. If you think that this is a mistake, contact the team at 'gustavo@gmail.com'.`nTo add vulnerabilities to suppression list (ignored list) you can set on ./cicd/dependency-check-config/suppression-backend.xml file. More info on FAQs in this repository README and in the official documentation.`nYou can discover more about OWASP Dependency Check in the official documentation at https://jeremylong.github.io/DependencyCheck/index.html"
 exit 1
