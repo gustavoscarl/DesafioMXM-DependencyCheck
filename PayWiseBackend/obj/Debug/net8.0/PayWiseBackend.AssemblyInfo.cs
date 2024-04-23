b5dfb6e0cfdcd8a195950062728c984b32a4ec62
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-04-22 22:36:03 -0300

    testing changes on dependencies files

--- PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
+++ PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64f34aec5df54783200aa4a077502bb151227f42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e2c591000c36ba414f0d467805cb076483846ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
