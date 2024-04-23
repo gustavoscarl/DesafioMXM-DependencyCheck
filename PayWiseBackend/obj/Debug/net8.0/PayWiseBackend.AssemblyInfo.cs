361d7134a1eeca5b10816f3334ac19657d6c8c00
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-04-23 13:14:10 -0300

    test: testing if cache worked

--- PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
+++ PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f411c96fc71e65602a0c82ac4d7299d1d19339d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+776c8e7dcc26dfa1cc4b6898bab6c680933c6e35")]
 [assembly: System.Reflection.AssemblyProductAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
