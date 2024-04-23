efb31de80008b9c7f370d7a605b8e738462ee8d8
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-04-23 14:44:22 -0300

    test: triggering workflow

--- PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
+++ PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dfdee0ed3a2c910606654b32469e2c0d84cf855")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b6aafa94fefae3ec4490e2d0933d1a70db6dc3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
