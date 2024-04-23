1bc2d54f6a5b5d791136d3c82c2481539007cbf0
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-04-23 16:46:19 -0300

    testing: merge to main

--- PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
+++ PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2ceb42a7c07dd81716301f20f40b3c5e1da47e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf2f5780af7f52b5a280aba8f87a9e227a705cc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
