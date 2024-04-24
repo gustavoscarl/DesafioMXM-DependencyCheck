f045ca4896a03b5911c42a081a41e109dfa7c163
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-04-24 16:08:42 -0300

    fix and feature: correções no arquivo de resultado de vulnerabilidades e adicionando variaveis de caminhos

--- .cicd/scripts/high_critical_vulnefound.ps1
+++ .cicd/scripts/high_critical_vulnefound.ps1
@@ -11,6 +11,6 @@ if ($altoNivel -eq "true") {
 if ($criticoNivel -eq "true") {
   Write-Output "[FALHA] Vulnerabilidades de Gravidade Crítica encontradas."
 }
-Write-Output "`nVerifique e atualize seus pacotes para corrigir a segurança do projeto. Se você acha que isso é um erro, entre em contato com a equipe em 'gustavo@gmail.com'.`nPara adicionar vulnerabilidades a lista de supressão (lista de falsos positivos), você pode defini-las em './cicd/dependency-check-config/supplession-xxx.xml'. Mais informações sobre perguntas frequentes podem ser encontradas no README deste repositório e na documentação oficial.`nVoce pode descobrir mais sobre a verificação de dependência da OWASP na documentação oficial em https://jeremylong.github.io/dependencycheck/index.html"
+Write-Output "`nVerifique e atualize seus pacotes para corrigir a segurança do projeto. Se você acha que isso é um erro, entre em contato com a equipe em 'gustavo@gmail.com'.`nPara adicionar vulnerabilidades a lista de supressão (lista de falsos positivos), você pode defini-las em './cicd/dependency-check-config/supplession-xxx.xml'. Mais informações sobre perguntas frequentes podem ser encontradas no README deste repositório e na documentação oficial.`nVocê pode descobrir mais sobre a verificação de dependência da OWASP na documentação oficial em https://jeremylong.github.io/DependencyCheck/"
 
 exit 1
