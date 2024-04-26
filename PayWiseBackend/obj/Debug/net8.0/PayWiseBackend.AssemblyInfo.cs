f06ad05b55745387ac2cb3ab0b59d029f6d186c2
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-04-26 13:34:04 -0300

    testando push backend

--- PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
+++ PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9e87f7c37a786e23921b04181b4de7d46166afe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce28e811f3b0baf34437165368b75de6b868e2d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
