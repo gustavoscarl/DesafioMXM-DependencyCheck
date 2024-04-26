e0b2550b09ae9308965d36ee16aaa4459d4f6401
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-04-26 18:34:34 -0300

    testando workflows branch test

--- PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
+++ PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f6ed0a3270632bc99af037a56ec493403b19ea6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4e2a693b701dbb770f136dd43fab026acd12aa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
