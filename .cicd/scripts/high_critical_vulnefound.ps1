2ae704a05676c498208a8e84978f8f9fcd3cea44
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-04-26 22:08:59 -0300

    pequenas correções para implementação correta do artifact

--- .cicd/scripts/high_critical_vulnefound.ps1
+++ .cicd/scripts/high_critical_vulnefound.ps1
@@ -2,7 +2,7 @@ param (
   [string]$altoNivel,
   [string]$criticoNivel,
   [string]$linkSupressao,
-  [string]$linkRelatorios
+  [string]$linkArtifact
 )
 
 Write-Output "Vulnerabilidades Alta ou Críticas encontradas, falhando a build."
@@ -13,6 +13,6 @@ if ($altoNivel -eq "true") {
 if ($criticoNivel -eq "true") {
   Write-Output "[FALHA] Vulnerabilidades de Gravidade Crítica encontradas."
 }
-Write-Output "Verifique e atualize seus pacotes para corrigir a segurança do projeto. Se você acha que isso é um erro, entre em contato com a equipe em 'gustavo@gmail.com'.`nPara adicionar vulnerabilidades à lista de supressão (lista de falsos positivos), você pode defini-lás em $linkSupressao . Mais informações sobre perguntas frequentes podem ser encontradas no README deste repositório e na documentação oficial.`nPara acessar os Relatórios JSON e HTML completos e nativos da ferramenta, acesse: $linkRelatorios `nVocê pode descobrir mais sobre a verificação de dependência da OWASP na documentação oficial em https://jeremylong.github.io/DependencyCheck/"
+Write-Output "Verifique e atualize seus pacotes para corrigir a segurança do projeto. Se você acha que isso é um erro, entre em contato com a equipe em 'gustavo@gmail.com'.`nPara adicionar vulnerabilidades à lista de supressão (lista de falsos positivos), você pode defini-lás em $linkSupressao . Mais informações sobre perguntas frequentes podem ser encontradas no README deste repositório e na documentação oficial.`nPara acessar os Relatórios JSON e HTML completos e nativos da ferramenta, acesse: $linkArtifact `nVocê pode descobrir mais sobre a verificação de dependência da OWASP na documentação oficial em https://jeremylong.github.io/DependencyCheck/"
 
 exit 1
