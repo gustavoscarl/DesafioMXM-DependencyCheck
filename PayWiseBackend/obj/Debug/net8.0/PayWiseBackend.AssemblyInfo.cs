7f84218e98b18a855ec4dc3da78c33777c01ed3f
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-04-27 18:07:29 -0300

    cache

--- PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
+++ PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc64f5767b03af6a608bfcef683809325cd30e18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c0d3ff74f583749ae6553d3fc289d8134a9424")]
 [assembly: System.Reflection.AssemblyProductAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
