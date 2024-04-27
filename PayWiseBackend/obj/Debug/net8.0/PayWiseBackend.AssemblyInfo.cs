45e4d885efa9f68561b01bfc210a539fa2f17933
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-04-27 20:12:09 -0300

    ativando workflows push

--- PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
+++ PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92160bc684ceacef7bc2a0a99b561d79e2965d07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8556ca444ab9ca26888783c1ddfd95d155adaba")]
 [assembly: System.Reflection.AssemblyProductAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
