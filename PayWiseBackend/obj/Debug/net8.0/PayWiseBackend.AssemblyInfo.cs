acfe18a3bd6873cf1f3156739c463e010a151996
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-04-28 14:11:58 -0300

    ativando mudanças por push

--- PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
+++ PayWiseBackend/obj/Debug/net8.0/PayWiseBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6a14ddf885bf4464953d6ee9f221db5d5cbbcf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49ea28990cb1eb87004606cda98fd0d45c59acdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PayWiseBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
