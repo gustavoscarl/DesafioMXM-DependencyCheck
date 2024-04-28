ab93e765788f6f59a90ab00a5753fa9471dfc029
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2024-04-28 12:29:37 -0300

    corrigido erro ortografico em mensagem

--- .cicd/scripts/high_critical_vulnefound.ps1
+++ .cicd/scripts/high_critical_vulnefound.ps1
@@ -19,7 +19,7 @@ Write-Output "Se você acha que isso é um erro, entre em contato com a equipe e
 Write-Output "Para adicionar vulnerabilidades à lista de supressão (lista de falsos positivos), você pode defini-lás em $linkSupressao"
 Write-Output "Mais informações sobre perguntas frequentes podem ser encontradas no README deste repositório e na documentação oficial"
 Write-Output "Para acessar os Relatórios JSON e HTML completos e nativos da ferramenta, acesse: $linkArtifact"
-Write-Output "Você também pode acessar os relatórios pelo do repositório em: $linkRelatorios"
+Write-Output "Você também pode acessar os relatórios pelo arquivo do repositório em: $linkRelatorios"
 Write-Output "Você pode descobrir mais sobre a verificação de dependência da OWASP na documentação oficial em https://jeremylong.github.io/DependencyCheck/"
 
 exit 1
